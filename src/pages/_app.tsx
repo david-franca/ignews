fc64e9d97afb5d40989b0c8dcc4044b74c4bbd8c
David França <davidemanuel.cf@gmail.com>
2022-05-19 11:23:04 -0300

    Next Auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import type { AppProps } from "next/app";
-import { Header } from "../components/Header";
 import "../styles/global.scss";
+
+import { SessionProvider as NextAuthProvider } from "next-auth/react";
+
+import { Header } from "../components/Header";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NextAuthProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthProvider>
   );
 }
 
--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 
-import * as packageInfo from "../../package.json";
+import packageInfo from "../../package.json";
 
 export const stripe = new Stripe(process.env.STRIPE_API_KEY ?? "", {
   apiVersion: "2020-08-27",
